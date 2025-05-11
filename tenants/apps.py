68d4aceb3601b74bfa35373d76090a929f11140d
duplxey <nick.tomazic@gmail.com>
2025-05-11 14:41:14 +0200

    automatically create/delete UserTenantPermissions on admin changes

--- tenants/apps.py
+++ tenants/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TenantsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tenants"
+
+    def ready(self):
+        from tenants import signals
+        signals.verify_import()
